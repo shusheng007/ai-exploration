4bd681fb057526493e6604d0020aa3a43d298076
shusheng007 <shusheng007wb@gmail.com>
2025-03-25 23:11:33 +0800

    spring ai learn

--- spring-ai-openai/src/main/java/top/shusheng007/springaiopenai/config/AiConfiguration.java
+++ spring-ai-openai/src/main/java/top/shusheng007/springaiopenai/config/AiConfiguration.java
@@ -1,11 +1,23 @@
 package top.shusheng007.springaiopenai.config;
 
+import org.springframework.ai.autoconfigure.openai.OpenAiEmbeddingProperties;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
+import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
 import org.springframework.ai.chat.memory.ChatMemory;
 import org.springframework.ai.chat.memory.InMemoryChatMemory;
+import org.springframework.ai.document.Document;
+import org.springframework.ai.embedding.EmbeddingModel;
+import org.springframework.ai.openai.OpenAiEmbeddingModel;
+import org.springframework.ai.openai.api.OpenAiApi;
+import org.springframework.ai.reader.TextReader;
+import org.springframework.ai.vectorstore.SimpleVectorStore;
+import org.springframework.ai.vectorstore.VectorStore;
+import org.springframework.ai.writer.FileDocumentWriter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 @Configuration
 public class AiConfiguration {
@@ -17,7 +29,24 @@ public class AiConfiguration {
 
     @Bean
     public ChatClient chatClient(ChatClient.Builder chatClientBuilder){
-        return chatClientBuilder.defaultAdvisors(new MessageChatMemoryAdvisor(chatMemory()))
+        return chatClientBuilder
+                .defaultSystem("You are a helpful assistant")
+                .defaultAdvisors(new SimpleLoggerAdvisor())
+                .defaultAdvisors(new MessageChatMemoryAdvisor(chatMemory()))
                 .build();
+    }
+
+
+
+    @Bean
+    public VectorStore vectorStore(EmbeddingModel embeddingModel){
+        SimpleVectorStore simpleVectorStore = SimpleVectorStore.builder(embeddingModel).build();
+
+        TextReader textReader = new TextReader("classpath:static/private_data.txt");
+        List<Document> documents = textReader.get();
+
+        simpleVectorStore.add(documents);
+
+        return simpleVectorStore;
     }
 }
--- spring-ai-openai/src/main/java/top/shusheng007/springaiopenai/service/AssistantAppService.java
+++ spring-ai-openai/src/main/java/top/shusheng007/springaiopenai/service/AssistantAppService.java
@@ -2,7 +2,10 @@ package top.shusheng007.springaiopenai.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.RetrievalAugmentationAdvisor;
 import org.springframework.ai.chat.model.ChatResponse;
+import org.springframework.ai.rag.retrieval.search.VectorStoreDocumentRetriever;
+import org.springframework.ai.vectorstore.VectorStore;
 import org.springframework.stereotype.Service;
 import top.shusheng007.springaiopenai.web.dto.MyChatRequest;
 import top.shusheng007.springaiopenai.web.dto.MyChatResponse;
@@ -14,9 +17,18 @@ import java.util.UUID;
 @Service
 public class AssistantAppService {
     private final ChatClient chatClient;
-
+    private final VectorStore vectorStore;
 
     public MyChatResponse chat(MyChatRequest myChatRequest) {
+
+        RetrievalAugmentationAdvisor retrievalAugmentationAdvisor = RetrievalAugmentationAdvisor.builder()
+                .documentRetriever(VectorStoreDocumentRetriever.builder()
+                        .similarityThreshold(0.60)
+                        .vectorStore(vectorStore)
+                        .build())
+                .build();
+
+
         String chatId = Optional
                 .ofNullable(myChatRequest.getChatId())
                 .orElse(UUID.randomUUID().toString());
@@ -26,6 +38,7 @@ public class AssistantAppService {
                 .advisors(advisorSpec ->
                         advisorSpec
                                 .param("chat_memory_conversation_id", chatId))
+                .advisors(retrievalAugmentationAdvisor)
                 .call()
                 .entity(new DeepSeekR1ModelOutputConverter());
 
