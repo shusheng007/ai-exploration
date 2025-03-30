d64e95bd1818dbd4cbe88f07946efa998b44f1be
shusheng007 <shusheng007wb@gmail.com>
2025-03-30 21:35:09 +0800

    spring ai learn

--- spring-ai-openai/src/main/java/top/shusheng007/springaiopenai/application/IngestionService.java
+++ spring-ai-openai/src/main/java/top/shusheng007/springaiopenai/application/IngestionService.java
@@ -10,21 +10,22 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
 @Slf4j
 @RequiredArgsConstructor
-@Component
-public class IngestionService implements CommandLineRunner {
+@Service
+public class IngestionService {
     private final VectorStore vectorStore;
 
     @Value("classpath:/static/private_data.txt")
     private Resource personInfo;
 
-    @Override
-    public void run(String... args) throws Exception {
-        var txtReader = new TextReader(personInfo);
+    public void ingestData() {
+        TextReader txtReader = new TextReader(personInfo);
         TextSplitter textSplitter = new TokenTextSplitter();
         vectorStore.accept(textSplitter.apply(txtReader.get()));
+
         log.info("Vector store loaded with data");
     }
 }
